51a3e0c3a370419ce5229c6e5d2e703cbe4c73c0
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2023-05-31 16:27:16 +0200

    patch: scss

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 import Table from "./components/Table";
 import { useInfiniteScroll } from "./components/hook/useInfiniteScroll";
 import useDraggableRows from "./components/hook/useDraggable";
+import styles from './components/styles/index.scss'
 
 export { Table, useInfiniteScroll, useDraggableRows }
