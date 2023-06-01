3fdc80c3f8ea18cd108850e3bf5cb8c7e5f4dc38
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2023-06-01 15:46:14 +0200

    patch export lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,9 @@
 import Table from "./components/Table";
 import { useInfiniteScroll } from "./components/hook/useInfiniteScroll";
-import useDraggableRows from "./components/hook/useDraggable";
+import { useDraggableRows } from "./components/hook/useDraggable";
 import styles from './components/styles/index.scss'
+import { useSort } from './components/hook/useSort'
+import { useSearch } from "./components/hook/useSearch";
 
-export { Table, useInfiniteScroll, useDraggableRows }
+
+export { Table, useInfiniteScroll, useDraggableRows, styles, useSearch, useSort }
